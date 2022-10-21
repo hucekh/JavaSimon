44af1081bf31548a071cb5e65454bf23b2caaa83
hucekh <116318906+hucekh@users.noreply.github.com>
2022-10-20 21:53:05 -0500

    Update Simon.java

--- src/Simon.java
+++ src/Simon.java
@@ -1,3 +1,22 @@
-public class Simon {
+import javafx.application.Application;
+	import javafx.fxml.FXMLLoader;
+	import javafx.scene.Parent;
+	import javafx.scene.Scene;
+	import javafx.stage.Stage;
 
-}
+	public class Simon extends Application {
+	   @Override
+	   public void start(Stage stage) throws Exception {
+	      Parent root = 
+	         FXMLLoader.load(getClass().getResource("Simon.fxml"));
+
+	      Scene scene = new Scene(root); // attach scene graph to scene
+	      stage.setTitle("Simon"); // displayed in window's title bar
+	      stage.setScene(scene); // attach scene to stage
+	      stage.show(); // display the stage
+	   }
+
+	   public static void main(String[] args) {
+	      launch(args);	// call start method
+	   }
+	}
