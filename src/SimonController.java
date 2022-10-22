cba8c5dd271e6563ee482318fb66efa9fbc7379a
hucekh <116318906+hucekh@users.noreply.github.com>
2022-10-21 21:23:16 -0500

    Update SimonController.java

--- src/SimonController.java
+++ src/SimonController.java
@@ -1,3 +1,32 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Arc;
+
 public class SimonController {
+
+    @FXML
+    private Arc yellowButton;
+
+    @FXML
+    private Arc purpleButton;
+
+    @FXML
+    private Arc redButton;
+
+    @FXML
+    private Arc greenButton;
+
+    @FXML
+    private Arc blueButton;
+
+    @FXML
+    private Arc orangeButton;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    private Label currentRound;
 
 }
