a2358b1434e55210f6dc375ec26cb57bd1065b92
hucekh <116318906+hucekh@users.noreply.github.com>
2022-10-23 18:42:04 -0500

    Update Simon.java

--- src/Simon.java
+++ src/Simon.java
@@ -23,15 +23,55 @@ import javafx.application.Application;
 	   
 	   private ArrayList<String> colorSequence;
 	   
-	   public Simon() {	// create Simon object
+	   public Simon() {	// constructor to create Simon object, creates arraylist for sequence
 		   
 	   }
-
-	   public void setSequence(ArrayList<String> colorSequence) {
+	   
+	   public void start() {	// starts game
+		   
+	   }
+	   
+	   public void generateSequence() {	// generates random sequence of colors by adding random colors to arraylist
+		   
+	   }
+	   
+	   public boolean checkInput() {	// checks if user selected the correct color
+		   
+	   }
+	   
+	   public void endGame() {	// ends game
+		   
+	   }
+	   
+	   public void addColorToSequence() {	// ads random color to sequence
+		   
+	   }
+	   
+	   public void randomColor() {	// returns random color
+		   
+	   }
+	   
+	   public int getIndex() {	// returns the current index
+		   
+	   }
+	   
+	   public void setIndex() {	// setter method for current index
+		   
+	   }
+	   
+	   public ArrayList<String> getSequence() {	// getter method for arraylist
+		return colorSequence;
+	   }
+	   
+	   public void setSequence(ArrayList<String> colorSequence) {	// sets sequence to an array of colors
 		   this.colorSequence = colorSequence;
 	   }
 	   
-	   public ArrayList<String> getSequence() {
-		return colorSequence;
-	}
+	   public isGameOn() {	// returns value of gameOn
+		   
+	   }
+	   
+	   public void setGameOn() {	// sets gameOn variable to boolean value
+		   
+	   }
 	}
