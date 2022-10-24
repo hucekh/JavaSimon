7e5019b0a5a434d447e30d96ee5ad02d73fba1d3
hucekh <116318906+hucekh@users.noreply.github.com>
2022-10-23 23:24:07 -0500

    Update Simon.java

--- src/Simon.java
+++ src/Simon.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 import java.lang.Math;
 import javafx.application.Application;
-	import javafx.fxml.FXMLLoader;
-	import javafx.scene.Parent;
-	import javafx.scene.Scene;
-	import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 	public class Simon extends Application {
 	   @Override
